c869e29963bf37138b02847ed065c0dd81439301
Jacob Pennington <jacob.p.neuro@gmail.com>
2024-10-18 23:37:57 -0400

    fix torch error for empty stt when all spikes negative for first batch

--- kilosort/template_matching.py
+++ kilosort/template_matching.py
@@ -38,23 +38,19 @@ def extract(ops, bfile, U, device=torch.device('cuda'), progress_bar=None):
     
     try:
         for ibatch in prog:
-            if ibatch % 100 == 0:
+            if ibatch % 100 == 0 and ibatch != 0:
                 log_performance(logger, 'debug', f'Batch {ibatch}')
 
             X = bfile.padded_batch_to_torch(ibatch, ops)
             stt, amps, Xres = run_matching(ops, X, U, ctc, device=device)
 
-            if len(stt.size()) == 0:
-                do_debug = False
-            else:
-                do_debug = True
-            if ibatch % 100 == 0 and do_debug:
+            if ibatch % 100 == 0 and ibatch != 0:
                 logger.debug(f'1: {torch.min(stt)}')
 
             xfeat = Xres[iCC[:, iU[stt[:,1:2]]],stt[:,:1] + tiwave] @ ops['wPCA'].T
             xfeat += amps * Ucc[:,stt[:,1]]
 
-            if ibatch == 0:
+            if ibatch == 0 and ibatch != 0:
                 # Can sometimes get negative spike times for first batch since
                 # we're aligning to nt0min, not nt//2, but these should be discarded.
                 neg_spikes = (stt[:,0] - nt - nt//2 + ops['nt0min']) < 0
@@ -65,19 +61,19 @@ def extract(ops, bfile, U, device=torch.device('cuda'), progress_bar=None):
             nsp = len(stt) 
             if k+nsp>st.shape[0]:                     
                 st = np.concatenate((st, np.zeros_like(st)), 0)
-                if ibatch % 100 == 0 and do_debug:
+                if ibatch % 100 == 0:
                     logger.debug(f'2: {(st[:,0]).min()}')
                 tF  = torch.cat((tF,  torch.zeros_like(tF)), 0)
 
             stt = stt.double()
             st[k:k+nsp,0] = ((stt[:,0]-nt) + ibatch * (ops['batch_size'])).cpu().numpy() - nt//2 + ops['nt0min']
-            if ibatch % 100 == 0 and do_debug:
+            if ibatch % 100 == 0 and ibatch != 0:
                 logger.debug(f'3: {st[:,0].min()}')
                 logger.debug(f'4: {torch.min((stt[:,0]-nt) + ibatch * (ops["batch_size"]))}')
                 logger.debug(f'5: {torch.min(stt[:,0]-nt)}')
             st[k:k+nsp,1] = stt[:,1].cpu().numpy()
             st[k:k+nsp,2] = amps[:,0].cpu().numpy()
-            if ibatch % 100 == 0 and do_debug:
+            if ibatch % 100 == 0 and ibatch != 0:
                 logger.debug(f'6: {(st[:,0]).min()}')
             
             tF[k:k+nsp]  = xfeat.transpose(0,1).cpu()
