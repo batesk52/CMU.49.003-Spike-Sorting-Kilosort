e67f9782476f7eaa0be071467a3c3518d72418e5
batesk52 <batesk52@gmail.com>
2024-11-19 22:30:50 -0500

    spinal cord pipeline fully functional!!!

--- automations/RM1.py
+++ automations/RM1.py
@@ -117,15 +117,28 @@ class Rat:
 
     def get_sc_data(self):
         for recording in self.intan_recordings:
-            self.sc_data[recording] = self.intan_recordings[recording].remove_channels(["B-000","B-001"])   
-
+            try:
+                self.sc_data[recording] = self.intan_recordings[recording].remove_channels(["B-000","B-001"])   
+            except:
+                self.sc_data[recording] = None  
+                print(f'Error recording SC data for {recording}')
     def get_nerve_cuff_data(self):
         for recording in self.intan_recordings:
-            self.nerve_cuff_data[recording] = self.intan_recordings[recording].select_channels(["B-000"])
+            try:
+                self.nerve_cuff_data[recording] = self.intan_recordings[recording].select_channels(["B-000"])
+            except:
+                self.nerve_cuff_data[recording] = None
+                print(f'Error recording nerve cuff data for {recording}')
+                pass
 
     def get_emg_data(self):
         for recording in self.intan_recordings:
-            self.emg_data[recording] = self.intan_recordings[recording].select_channels(["B-001"])
+            try:
+                self.emg_data[recording] = self.intan_recordings[recording].select_channels(["B-001"])
+            except:
+                self.emg_data[recording] = None
+                print(f'Error recording emg data for {recording}')
+                pass
 
 class kilosort_wrapper:
     def __init__(self, rat_class_instance, SAVE_DIRECTORY): # for this class, you will pass the class instance of a rat object as an argument
@@ -134,7 +147,7 @@ class kilosort_wrapper:
         self.PROBE_DIRECTORY = rat_class_instance.PROBE_DIRECTORY
         self.DATA_DIRECTORY = rat_class_instance.DATA_DIRECTORY
         self.SAVE_DIRECTORY = SAVE_DIRECTORY
-        print(self.RAT_ID)
+        print(f"running kilosort wrapper for rat {self.RAT_ID}")
 
     def save_spinalcord_data_to_binary(self, TRIAL_NAMES = None):
         # NOTE: Data will be saved as np.int16 by default since that is the standard
@@ -144,7 +157,7 @@ class kilosort_wrapper:
         # these files will be saved to the subfolder: SAVE_DIRECTORY/binary/TRIAL_NAMES
 
         if TRIAL_NAMES == None: # if nothing is passed, it will run through all of the trials in the rat object
-            # try:
+            try:
                 for recording in self.data.sc_data:
                     dtype = np.int16
                     filename, N, c, s, fs, probe_path = io.spikeinterface_to_binary(
@@ -152,20 +165,18 @@ class kilosort_wrapper:
                         chunksize=60000, export_probe=False, probe_name= self.PROBE_DIRECTORY # export the probe file is false because there is no probe file to begin with
                         )
                     print(f'Data saved to {filename}')
-            # except:
-                # print(f'ERROR: No data found for {recording}')
+            except:
+                print(f'ERROR: issue importing data for {recording}')
         else:
-            # try:
+            try:
                 for recording in TRIAL_NAMES:
                     dtype = np.int16
                     filename, N, c, s, fs, probe_path = io.spikeinterface_to_binary(
                         self.data.sc_data[recording], os.path.join(self.SAVE_DIRECTORY, 'binary',f"{recording}"), data_name=f'{self.RAT_ID}_{recording}_data.bin', dtype=dtype,
                         chunksize=60000, export_probe=False, probe_name= self.PROBE_DIRECTORY # export the probe file is false because there is no probe file to begin with
                         )
-            # except:
-                # print(f'ERROR: No data found for {recording}')
-
-
+            except:
+                print(f'ERROR: issue importing data for {recording}')
 
     def run_spinalcord_kilosort(self, new_settings = None, **kwargs): # runs kilosort in a loop through all of the binary files saved to the SAVE_DIRECTORY/binary folder
 
@@ -178,90 +189,95 @@ class kilosort_wrapper:
                 folders_with_bin.append(root)
 
         for folder in folders_with_bin:
-            print(f'Running kilosort on {folder}')
+            print(f'|\n|\n|\n|\n|\n|\nRunning kilosort on {os.path.basename(folder)}\n|\n|\n|\n|\n|\n|')
 
-            # NOTE: 'n_chan_bin' is a required setting, and should reflect the total number
-            #       of channels in the binary file. For information on other available
-            #       settings, see `kilosort.run_kilosort.default_settings`.
+            try:
+                    
+                # NOTE: 'n_chan_bin' is a required setting, and should reflect the total number
+                #       of channels in the binary file. For information on other available
+                #       settings, see `kilosort.run_kilosort.default_settings`.
 
-            if new_settings is None:
-                settings = DEFAULT_SETTINGS
-                settings = {'data_dir': os.path.join(self.SAVE_DIRECTORY, 'binary', folder), 'n_chan_bin': 32}
-            
-            else:
-                settings = DEFAULT_SETTINGS
-                settings = new_settings
+                if new_settings is None:
+                    settings = DEFAULT_SETTINGS
+                    settings = {'data_dir': os.path.join(self.SAVE_DIRECTORY, 'binary', folder), 'n_chan_bin': 32}
+                
+                else:
+                    settings = DEFAULT_SETTINGS
+                    settings = new_settings
 
-            ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = \
-                run_kilosort(
-                    settings=settings, probe_name=self.PROBE_DIRECTORY,
-                    # save_preprocessed_copy=True
-                    )
-            
-            # outputs saved to results_dir
-            results_dir = Path(settings['data_dir']).joinpath('kilosort4')
-            ops = np.load(results_dir / 'ops.npy', allow_pickle=True).item()
-            camps = pd.read_csv(results_dir / 'cluster_Amplitude.tsv', sep='\t')['Amplitude'].values
-            contam_pct = pd.read_csv(results_dir / 'cluster_ContamPct.tsv', sep='\t')['ContamPct'].values
-            chan_map =  np.load(results_dir / 'channel_map.npy')
-            templates =  np.load(results_dir / 'templates.npy')
-            chan_best = (templates**2).sum(axis=1).argmax(axis=-1)
-            chan_best = chan_map[chan_best]
-            amplitudes = np.load(results_dir / 'amplitudes.npy')
-            st = np.load(results_dir / 'spike_times.npy')
-            clu = np.load(results_dir / 'spike_clusters.npy')
-            firing_rates = np.unique(clu, return_counts=True)[1] * 30000 / st.max()
-            dshift = ops['dshift']
+                ops, st, clu, tF, Wall, similar_templates, is_ref, est_contam_rate, kept_spikes = \
+                    run_kilosort(
+                        settings=settings, probe_name=self.PROBE_DIRECTORY,
+                        # save_preprocessed_copy=True
+                        )
+                
+                # outputs saved to results_dir
+                results_dir = Path(os.path.join(self.SAVE_DIRECTORY, 'binary',os.path.basename(folder), 'kilosort4')) # Path(settings['data_dir']).joinpath('kilosort4')
+                ops = np.load(results_dir / 'ops.npy', allow_pickle=True).item()
+                camps = pd.read_csv(results_dir / 'cluster_Amplitude.tsv', sep='\t')['Amplitude'].values
+                contam_pct = pd.read_csv(results_dir / 'cluster_ContamPct.tsv', sep='\t')['ContamPct'].values
+                chan_map =  np.load(results_dir / 'channel_map.npy')
+                templates =  np.load(results_dir / 'templates.npy')
+                chan_best = (templates**2).sum(axis=1).argmax(axis=-1)
+                chan_best = chan_map[chan_best]
+                amplitudes = np.load(results_dir / 'amplitudes.npy')
+                st = np.load(results_dir / 'spike_times.npy')
+                clu = np.load(results_dir / 'spike_clusters.npy')
+                firing_rates = np.unique(clu, return_counts=True)[1] * 30000 / st.max()
+                dshift = ops['dshift']
 
-            rcParams['axes.spines.top'] = False
-            rcParams['axes.spines.right'] = False
-            gray = .5 * np.ones(3)
+                rcParams['axes.spines.top'] = False
+                rcParams['axes.spines.right'] = False
+                gray = .5 * np.ones(3)
 
-            fig = plt.figure(figsize=(10,10), dpi=100)
-            grid = gridspec.GridSpec(3, 3, figure=fig, hspace=0.5, wspace=0.5)
+                fig = plt.figure(figsize=(10,10), dpi=100)
+                fig.suptitle(os.path.basename(folder))
+                grid = gridspec.GridSpec(3, 3, figure=fig, hspace=0.5, wspace=0.5)
 
-            ax = fig.add_subplot(grid[0,0])
-            ax.plot(np.arange(0, ops['Nbatches'])*2, dshift);
-            ax.set_xlabel('time (sec.)')
-            ax.set_ylabel('drift (um)')
+                ax = fig.add_subplot(grid[0,0])
+                ax.plot(np.arange(0, ops['Nbatches'])*2, dshift);
+                ax.set_xlabel('time (sec.)')
+                ax.set_ylabel('drift (um)')
 
-            ax = fig.add_subplot(grid[0,1:])
-            t0 = 0
-            t1 = np.nonzero(st > ops['fs']*5)[0][0]
-            ax.scatter(st[t0:t1]/30000., chan_best[clu[t0:t1]], s=0.5, color='k', alpha=0.25)
-            ax.set_xlim([0, 5])
-            ax.set_ylim([chan_map.max(), 0])
-            ax.set_xlabel('time (sec.)')
-            ax.set_ylabel('channel')
-            ax.set_title('spikes from units')
+                ax = fig.add_subplot(grid[0,1:])
+                t0 = 0
+                t1 = np.nonzero(st > ops['fs']*5)[0][0]
+                ax.scatter(st[t0:t1]/30000., chan_best[clu[t0:t1]], s=0.5, color='k', alpha=0.25)
+                ax.set_xlim([0, 5])
+                ax.set_ylim([chan_map.max(), 0])
+                ax.set_xlabel('time (sec.)')
+                ax.set_ylabel('channel')
+                ax.set_title('spikes from units')
 
-            ax = fig.add_subplot(grid[1,0])
-            nb=ax.hist(firing_rates, 20, color=gray)
-            ax.set_xlabel('firing rate (Hz)')
-            ax.set_ylabel('# of units')
-
-            ax = fig.add_subplot(grid[1,1])
-            nb=ax.hist(camps, 20, color=gray)
-            ax.set_xlabel('amplitude')
-            ax.set_ylabel('# of units')
-
-            ax = fig.add_subplot(grid[1,2])
-            nb=ax.hist(np.minimum(100, contam_pct), np.arange(0,105,5), color=gray)
-            ax.plot([10, 10], [0, nb[0].max()], 'k--')
-            ax.set_xlabel('% contamination')
-            ax.set_ylabel('# of units')
-            ax.set_title('< 10% = good units')
-
-            for k in range(2):
-                ax = fig.add_subplot(grid[2,k])
-                is_ref = contam_pct<10.
-                ax.scatter(firing_rates[~is_ref], camps[~is_ref], s=3, color='r', label='mua', alpha=0.25)
-                ax.scatter(firing_rates[is_ref], camps[is_ref], s=3, color='b', label='good', alpha=0.25)
-                ax.set_ylabel('amplitude (a.u.)')
+                ax = fig.add_subplot(grid[1,0])
+                nb=ax.hist(firing_rates, 20, color=gray)
                 ax.set_xlabel('firing rate (Hz)')
-                ax.legend()
-                if k==1:
-                    ax.set_xscale('log')
-                    ax.set_yscale('log')
-                    ax.set_title('loglog')
-        
+                ax.set_ylabel('# of units')
+
+                ax = fig.add_subplot(grid[1,1])
+                nb=ax.hist(camps, 20, color=gray)
+                ax.set_xlabel('amplitude')
+                ax.set_ylabel('# of units')
+
+                ax = fig.add_subplot(grid[1,2])
+                nb=ax.hist(np.minimum(100, contam_pct), np.arange(0,105,5), color=gray)
+                ax.plot([10, 10], [0, nb[0].max()], 'k--')
+                ax.set_xlabel('% contamination')
+                ax.set_ylabel('# of units')
+                ax.set_title('< 10% = good units')
+
+                for k in range(2):
+                    ax = fig.add_subplot(grid[2,k])
+                    is_ref = contam_pct<10.
+                    ax.scatter(firing_rates[~is_ref], camps[~is_ref], s=3, color='r', label='mua', alpha=0.25)
+                    ax.scatter(firing_rates[is_ref], camps[is_ref], s=3, color='b', label='good', alpha=0.25)
+                    ax.set_ylabel('amplitude (a.u.)')
+                    ax.set_xlabel('firing rate (Hz)')
+                    ax.legend()
+                    if k==1:
+                        ax.set_xscale('log')
+                        ax.set_yscale('log')
+                        ax.set_title('loglog')
+            except:
+                print(f'ERROR: could not analyze {os.path.basename(folder)}')
+                pass
